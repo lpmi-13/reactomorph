eb84ffa1ac8d8998a6d86dbdd1758f7433d0c416
lpmi-13 <leskis@gmail.com>
2018-08-10 07:57:38 +0100

    remove extra text for now

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,6 @@ const Home = () => {
     <div>
       <p>Sometimes, it's nice to know that a word like "unite" is related to other words like "unity", "union", and "unionize".</p>
       <p>On the "Search" page, you can search for words like this and see the related words.</p>
-      <p>On the "List" page you can see a list of all the related words.</p>
     </div>
   );
 }
